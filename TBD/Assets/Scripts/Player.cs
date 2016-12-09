7362ed3bfc41b3907c4eb53ed4d098aaad229f02
trauble <ga78qew@mytum.de>
2016-12-09 22:08:01 +0100

    igig

---
+++ TBD/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+    public Camera[] cams;
+    private int numberOfPlayers;
+
+	// Use this for initialization
+	void Start () {
+        CmdConnection();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    [Command]
+    public void CmdConnection()
+    {
+        RpcCameraChange(numberOfPlayers);
+        numberOfPlayers++;
+    }
+
+    [ClientRpc]
+    public void RpcCameraChange(int i)
+    {
+        if (isLocalPlayer)
+        {
+            cams[i].enabled = true;
+        }
+    }
+}
