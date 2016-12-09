51fd5b693e34512b763c22646095b2e79df050ea
Liou <liou@gmx.at>
2016-12-09 23:55:11 +0100

    camfollow,move

---
+++ TBD/Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 10.0f;
+    public float rotSpeed = 100.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float move = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float rot = Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+
+        transform.Translate(0, 0, move);
+        transform.Rotate(0, rot, 0);
+    }
+}
