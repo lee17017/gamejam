2493834ee244282e75d0044e341a656f87268690
flor3tt <flre@gmx.net>
2016-12-10 02:43:06 +0100

    Shields v1

--- TBD/Assets/Scripts/Shields.cs
+++ TBD/Assets/Scripts/Shields.cs
@@ -7,6 +7,54 @@ public class Shields : Action {
 
     public override void Move()
     {
-        base.Move();
+        float deltaRot = player.ship.shieldRotSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        CmdSetShieldRotation(player.ship.shielRotation + deltaRot);
+
+        if(Input.GetButtonDown("Space") && player.ship.shieldActivated)
+        {
+            CmdTurnOffShield();            
+        }
+        if(Input.GetMouseButtonDown(1) && !player.ship.shieldActivated)
+        {
+            Vector3 mPos = Input.mousePosition;
+
+        }
+    }
+
+    [Command]
+    public void CmdSetShieldRotation(float degree)
+    {
+        ((Shields)player.actions[2]).RpcSetShieldRotation(degree);
+    }
+
+    [ClientRpc]
+    public void RpcSetShieldRotation(float degree)
+    {
+        player.ship.setShieldRotation(degree);
+    }
+
+    [Command]
+    public void CmdTurnOffShield()
+    {
+        ((Shields)player.actions[2]).RpcTurnOffShield();
+    }
+
+    [ClientRpc]
+    public void RpcTurnOffShield()
+    {
+        player.ship.shieldActivated = false;
+    }
+
+    [Command]
+    public void CmdTurnOnShield(float degree)
+    {
+        ((Shields)player.actions[2]).RpcTurnOnShield(degree);
+    }
+
+    [ClientRpc]
+    public void RpcTurnOnShield(float degree)
+    {
+        player.ship.shieldActivated.turnOnShield();
+        player.ship.setShieldRotation(degree);
     }
 }
