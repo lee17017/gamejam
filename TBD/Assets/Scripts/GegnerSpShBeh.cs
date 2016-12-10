272a4571c72b5ef78a626299ec8206462766970c
jungo <ga45jas@mytum.de>
2016-12-10 23:09:11 +0100

    Gegnergrundlagen

--- TBD/Assets/Scripts/GegnerSpShBeh.cs
+++ TBD/Assets/Scripts/GegnerSpShBeh.cs
@@ -8,17 +8,29 @@ public class GegnerSpShBeh : NetworkBehaviour {
 	// Use this for initialization
     int speed;
     int health;
+    public float radius;
+    bool attacking=false;
+    GameObject Projectile;
+
 	void Start () {
-        //Spawneinstellungen
-        Transform spaceship = GameObject.Find("SpaceShip").GetComponent<SpaceShip>().transform;
-        transform.LookAt(spaceship);
+        //Projectile
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //if(genug abstand)
-        transform.position = transform.position + transform.forward * speed * Time.deltaTime;
-        //else{//fly in circles around the player and shoot!
+        Transform spaceship = GameObject.Find("SpaceShip").GetComponent<SpaceShip>().transform;
+        transform.LookAt(spaceship);
+
+        if (Vector2.Distance(new Vector2(gameObject.transform.position.x, gameObject.transform.position.z), new Vector2(spaceship.transform.position.x, spaceship.transform.position.z)) <= radius)
+        {
+            attacking = true;
+            StartCoroutine(Attack());
+        }
+        else if(!attacking)
+        {
+            transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+        }
+
 	}
     private void OnTriggerEnter(Collider other)
     {
@@ -28,13 +40,41 @@ public class GegnerSpShBeh : NetworkBehaviour {
             return;
         }
 
-        if (other.tag == "bullet")
+        if (other.tag == "bullet"&&other.GetComponent<Bullet>().friendly)
         {
             Destroy(gameObject);
         }
         else if (other.tag == "SpaceShip")
         {
 
+            other.GetComponentInParent<SpaceShip>().player.takeDamage(10);
         }
+        else if (other.tag == "shield")
+        {
+            Destroy(gameObject);
+        }
+    }
+    IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(0.5f);
+        GameObject tmp= (GameObject)Instantiate(Projectile, gameObject.transform);
+        tmp.GetComponent<Bullet>().friendly = false;
+        //Evtl Positionsanpassung nötig
+        yield return new WaitForSeconds(1f);
+        tmp = (GameObject)Instantiate(Projectile, gameObject.transform);
+        tmp.GetComponent<Bullet>().friendly = false;
+
+        yield return new WaitForSeconds(1f);
+        tmp = (GameObject)Instantiate(Projectile, gameObject.transform);
+        tmp.GetComponent<Bullet>().friendly = false;
+
+        yield return new WaitForSeconds(1f);
+        tmp = (GameObject)Instantiate(Projectile, gameObject.transform);
+        tmp.GetComponent<Bullet>().friendly = false;
+
+        yield return new WaitForSeconds(1f);
+        tmp = (GameObject)Instantiate(Projectile, gameObject.transform);
+        tmp.GetComponent<Bullet>().friendly = false;
+
     }
 }
