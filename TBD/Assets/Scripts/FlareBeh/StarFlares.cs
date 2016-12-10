a752f030b70e86dd6cf9a8d137531b9bcfc99a27
flor3tt <flre@gmx.net>
2016-12-10 23:33:54 +0100

    bessere lens flares

--- TBD/Assets/Scripts/FlareBeh/StarFlares.cs
+++ TBD/Assets/Scripts/FlareBeh/StarFlares.cs
@@ -10,16 +10,21 @@ public class StarFlares : MonoBehaviour {
 	void Start ()
     {
         GameObject parent = (GameObject)Instantiate(empty);
+        Vector3 pos;
         for (int i = 0; i < 350; i++)
         {
-            GameObject g = (GameObject)Instantiate(starFabs[Random.Range(0, 3)], Random.onUnitSphere * 1000, Quaternion.identity);
+            pos = Random.insideUnitSphere * 2000;
+            pos.y = -Mathf.Abs(pos.y);
+            GameObject g = (GameObject)Instantiate(starFabs[Random.Range(0, 3)], pos, Quaternion.identity);
             g.transform.SetParent(parent.transform);
             g.GetComponent<LensFlare>().brightness = Random.Range(0.01f, 0.8f);
             g.GetComponent<LensFlare>().color = Random.ColorHSV();
         }
         for (int i = 0; i < 150; i++)
         {
-            GameObject g = (GameObject)Instantiate(starFabs[Random.Range(0, 3)], Random.onUnitSphere * 200, Quaternion.identity);
+            pos = Random.insideUnitSphere * 200;
+            pos.y = -Mathf.Abs(pos.y);
+            GameObject g = (GameObject)Instantiate(starFabs[Random.Range(0, 4)], pos, Quaternion.identity);
             g.transform.SetParent(parent.transform);
             g.GetComponent<LensFlare>().brightness = Random.Range(0.01f, 0.8f);
             g.GetComponent<LensFlare>().color = Random.ColorHSV();
