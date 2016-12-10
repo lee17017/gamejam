523f4836da64179ca1827c5eded851ff13066657
Liou <liou@gmx.at>
2016-12-10 11:48:30 +0100

    shoot to player2 + cd

--- TBD/Assets/Scripts/SpaceShip.cs
+++ TBD/Assets/Scripts/SpaceShip.cs
@@ -8,7 +8,8 @@ public class SpaceShip : MonoBehaviour {
     public Camera[] cams;
     public Transform shieldRota;
     public GameObject shield;
-
+    public float coolDown = 2.0f;
+    public float timer=0;
     //Shield Control variables
     public bool shieldActivated;
     public float shieldRotSpeed;
@@ -18,6 +19,8 @@ public class SpaceShip : MonoBehaviour {
     {
         shieldRota.rotation = Quaternion.Euler(0,90 - shieldRotation,0);        
         shield.active = shieldActivated;
+        if (timer > 0)
+            timer -= Time.deltaTime;
     }
 
     public IEnumerator turnOnShield()
