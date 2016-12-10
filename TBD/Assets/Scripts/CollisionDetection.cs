59efb6eeba0a7c752cced0dfe641c88f325e4e92
Liou <liou@gmx.at>
2016-12-10 13:21:34 +0100

    detection

---
+++ TBD/Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "bullet")
+            Debug.Log("AAAHAADSFASDHFAHSDHFDSH");  
+    }
+}
