21a9e2c0152128170dd736952f44a28912eb1922
Jonathan <jjjooonnn@gmail.com>
2016-12-10 10:52:37 +0100

    Replaced faulty Mesh with another working one. Fixed Cannon Prefab. Tweaked Cannon related scripts.

--- TBD/Assets/Scripts/Base.cs
+++ TBD/Assets/Scripts/Base.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Base : MonoBehaviour {
     float yaw;
     float mouseSensitivity = 5f;
+    float targetRot;
+    public float defaultX = -90f;
+    public float range;
 
     // Use this for initialization
     void Start()
     {
-
+        targetRot = transform.localEulerAngles.z;
     }
 
     // Update is called once per frame
@@ -18,7 +21,14 @@ public class Base : MonoBehaviour {
         //get value
         yaw = Input.GetAxis("Mouse X") * mouseSensitivity;
 
+        //combine
+        targetRot += yaw;
+
+        //limit angle
+        targetRot = Mathf.Clamp(targetRot, -range, range);
+
         //rotate
-        transform.Rotate(0,0,yaw);
+        //transform.Rotate(0,0,yaw);
+        transform.localRotation = Quaternion.Euler(defaultX, 0f, targetRot);
     }
 }
