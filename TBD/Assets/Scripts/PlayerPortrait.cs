ed5e4882639207c6e84499d9d9a4bc2860572178
Boris-Chengbiao Zhou <bobo1239@web.de>
2016-12-10 17:37:49 +0100

    PlayerPortrait

---
+++ TBD/Assets/Scripts/PlayerPortrait.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Common;
+
+public class PlayerPortrait : MonoBehaviour {
+	public Image image;
+	private Role role = Role.None;
+	public Sprite[] roleImages;
+
+	void Start() {
+		role = Role.None;
+		image = this.GetComponentInParent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(1)) {
+			SetRole (Role.Move);
+		}
+	}
+
+	public void SetRole(Role role) {
+		this.role = role;
+		image = this.GetComponentInParent<Image> ();
+		image.color = Color.white;
+		image.sprite = roleImages [(int)role];
+	}
+
+	public Role getRole() {
+		return role;
+	}
+}
